9a392f85e1b2871773484debe1df20288c92169b
CesarSP99 <cesarandres00@gmail.com>
2021-08-20 19:42:17 -0400

    Add Home Page view

---
+++ lib/views/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import '../widgets/custom_text_button.dart';
+import '../utils/const.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Map<String, dynamic>> options = [
+    {
+      'text': 'Manual',
+      'icon': Icons.edit,
+    },
+    {
+      'text': 'QR',
+      'icon': Icons.qr_code,
+    },
+  ];
+
+  void onManualPressed() {
+    print('hola');
+  }
+
+  void onQRPressed() {
+    print('hola');
+  }
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      backgroundColor: Constants.backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: OvalBottomBorderClipper(),
+            child: Container(
+              color: Theme.of(context).accentColor,
+              height: 130 + statusBarHeight,
+            ),
+          ),
+          Positioned(
+            right: -45,
+            top: -30,
+            child: ClipOval(
+              child: Container(
+                color: Colors.black.withOpacity(0.05),
+                height: 150,
+                width: 150,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(Constants.paddingSide),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 30),
+                Text(
+                  'Validador de Carnet de Vacunación',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white),
+                ),
+                SizedBox(height: 70),
+                Container(
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CustomTextButton(
+                          icon: options[0]['icon'],
+                          text: options[0]['text'],
+                          onPressedAction: onManualPressed,
+                        ),
+                        CustomTextButton(
+                          icon: options[1]['icon'],
+                          text: options[1]['text'],
+                          onPressedAction: onQRPressed,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
