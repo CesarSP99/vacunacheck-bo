345eb03115e5e33d76c0d330334649b3f6e102f2
CesarSP99 <cesarandres00@gmail.com>
2021-08-20 22:00:05 -0400

    Add logic to prevent screen rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'utils/const.dart';
 import 'views/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
