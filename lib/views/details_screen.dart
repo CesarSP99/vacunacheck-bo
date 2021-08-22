2e269d7a665ee209ff0cb8fe726897729a2ae7cb
CesarSP99 <cesarandres00@gmail.com>
2021-08-22 18:34:58 -0400

    Add Details screen

---
+++ lib/views/details_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import '../utils/const.dart';
+
+class Details extends StatelessWidget {
+  Details({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      backgroundColor: Constants.backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: OvalBottomBorderClipper(),
+            child: Container(
+              color: Theme.of(context).accentColor,
+              height: 130 + statusBarHeight,
+            ),
+          ),
+          Positioned(
+            right: -45,
+            top: -30,
+            child: ClipOval(
+              child: Container(
+                color: Colors.black.withOpacity(0.05),
+                height: 150,
+                width: 150,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(Constants.paddingSide),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 30),
+                Text(
+                  'Detalles del vacunado',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white),
+                ),
+                SizedBox(height: 70),
+                Container(
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
