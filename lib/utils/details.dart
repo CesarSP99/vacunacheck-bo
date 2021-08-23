c3adb0d26d6d3a313bb0fc5c11bc45dfbed2499d
CesarSP99 <cesarandres00@gmail.com>
2021-08-23 00:21:32 -0400

    Add Details class

---
+++ lib/utils/details.dart
@@ -0,0 +1,31 @@
+class Details {
+  String? nombreCompleto;
+  String? ci;
+  String? fechaDeNacimiento;
+  String? departamento;
+  String? municipio;
+  String? establecimiento;
+  String? fechaDeVacunacion;
+  String? vacuna;
+  String? dosis;
+  String? proveedor;
+  String? lote;
+  String? consentimiento;
+  String? fechaProximaDosis;
+
+  Details({
+    this.nombreCompleto,
+    this.ci,
+    this.fechaDeNacimiento,
+    this.departamento,
+    this.municipio,
+    this.establecimiento,
+    this.fechaDeVacunacion,
+    this.vacuna,
+    this.dosis,
+    this.proveedor,
+    this.lote,
+    this.consentimiento,
+    this.fechaProximaDosis,
+  });
+}
